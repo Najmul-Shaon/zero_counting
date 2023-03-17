2f11357ac596f03aea38b4427f39c4ab47ab242b
Najmul-Shaon <103214871+Najmul-Shaon@users.noreply.github.com>
2023-03-17 23:36:50 +0600

    first commit

---
+++ number_zero_counting.js
@@ -0,0 +1,12 @@
+/*
+Complete the function called countZeros() which will take a binary string (Binary string is a string which consists of only 0 and 1) as a parameter and count how many 0’s are there in that string.
+*/
+
+function countZeros(binary_num) {
+    //Write your code here
+    //Don't forget to return
+    const binary_num_string = binary_num;
+    console.log(binary_num_string, typeof binary_num_string);
+    
+}
+countZeros("10")
